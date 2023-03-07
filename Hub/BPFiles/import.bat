c58f1b788ed9e4b8cd492e98c6b67cbd43a61466
soumik25sen <110536052+soumik25sen@users.noreply.github.com>
2023-03-07 13:22:43 +0100

    Update import.bat

--- Hub/BPFiles/import.bat
+++ Hub/BPFiles/import.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease  %1 /overwrite /user %2 %3 /dbconname "Jenkins POC"
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease %1 /overwrite /sso /dbconname "BluePrism Development Application Server"
